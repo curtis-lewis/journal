31f56123514a56bedd2649f6bef45e0dbf51a86b
Curtis Lewis <lewis.curtis.t@gmail.com>
2018-03-24 17:50:14 -0600

    added server functionality

--- getEntry.js
+++ getEntry.js
@@ -1,11 +1,43 @@
 function getEntry(req, res) {
-	var date = req.query.date;
-	var rating = req.query.rating;
-	var entry = req.query.entry;
+	const { Pool, Client } = require('pg')
+
+	const pool = new Pool({
+		user: 'j_user',
+		host: 'localhost',
+		database: 'journal',
+		password: 'j_pass',
+		port: 5432,
+	})
+
+	const client = new Client({
+		user: 'j_user',
+		host: 'localhost',
+		database: 'journal',
+		password: 'j_pass',
+		port: 5432,
+	})
+	client.connect()
+		var date = req.query.date;
+		var rating = req.query.rating;
+		var entry = req.query.entry;
 
 	var data = {date: date, rating: rating, entry: entry};
 	
 	res.render('display', data);
+
+	const query = {
+		name: 'insert-entry',
+		text: 'INSERT INTO ENTRY (user_id, post_date, rating, content) VALUES ($1, $2, $3, $4)',
+		values: [1,  date, rating, entry]
+	}
+
+	client.query(query, (err, res) => {
+		if (err) {
+			console.log(err.stack)
+		} else {
+			console.log(res.rows)
+		}
+	})
 }
 
 module.exports = {getEntry: getEntry};
--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+	user: 'j_user',
+	host: 'localhost',
+	database: 'journal',
+	password: 'j_pass',
+	port: 5432,
+})
+
+const client = new Client({
+	user: 'j_user',
+	host: 'localhost',
+	database: 'journal',
+	password: 'j_pass',
+	port: 5432,
+})
+client.connect()
+
 var getEntry = require('./getEntry.js')
 
 express()

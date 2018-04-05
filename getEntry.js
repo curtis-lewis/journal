cc40bb2027763e8a2b07cbe9bdda205119626505
Curtis Lewis <lewis.curtis.t@gmail.com>
2018-04-05 10:48:46 -0600

    bug fix

--- getEntry.js
+++ getEntry.js
@@ -27,7 +27,7 @@ function getEntry(req, res) {
 
 	client.connect();;
 
-	client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+	client.query('SELECT * FROM entry;', (err, res) => {
   		if (err) throw err;
   		for (let row of res.rows) {
     		console.log(JSON.stringify(row));
